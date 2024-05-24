3d5d4f0ba75fd23108487f2acc712be773873ba9
Jai Khurana <k.jai.khurana@gmail.com>
2024-05-25 03:31:02 +0530

    add demo and end of script

--- Assets/Scripts/DialogHandler/DialogHandler.cs
+++ Assets/Scripts/DialogHandler/DialogHandler.cs
@@ -11,6 +11,11 @@ public class DialogHandler : MonoBehaviour
     [SerializeField] private GameObject canvas;
     [SerializeField] private GameObject genralTextBox;
     [SerializeField] private Texture empty;
+    private int i = 0;
+
+    private List<string> dialogs = new List<string>();
+    private List<int> charintexes = new List<int>();
+    private List<int> emoindexes = new List<int>();
 
     private TMP_Text tmp;
 
@@ -18,15 +23,15 @@ public class DialogHandler : MonoBehaviour
     [SerializeField] private List<Texture> vasuliEmo = new List<Texture>();
     [SerializeField] private List<Texture> ngoEmo = new List<Texture>();
     [SerializeField] private List<Texture> oppEmo = new List<Texture>();
-    
+
     // IF YOU CHANGE NAMES OF ANY CHARACTERS ABOVE CHANGE THEM IN VOID START AND SWITCH CASE IN VOID DIALOG ALSO
     // ADD NEW LIST FOR A NEW CHARACTER
     // REFERENCE TEXTURES IN THE INSPECTOR TO BE USED
-    
+
     public List<List<Texture>> chardata = new List<List<Texture>>();
-    
+
     // FULL MATRIX OF ALL CHARACTERS AND EMOTIONAL STATES
-    
+
     // THE DIALOG FUNCTION CAN BE USED DIRECTLY IF A CANVAS IS PRESENT IN THE SCENE
     public void Dialog(string dialog, int characterIndex, int emoIndex)
     {
@@ -50,11 +55,12 @@ public class DialogHandler : MonoBehaviour
                 Debug.Log("CharacterIndex is Wrong");
                 break;
         }
-        
+
         var dialogbox = GameObject.FindWithTag("DialogBox");
         if (dialogbox == null)
         {
-            var dialogBox = Instantiate(genralTextBox, canvas.transform);
+            var genral = Instantiate(genralTextBox, canvas.transform);
+            var dialogBox = GameObject.FindWithTag("DialogBox");
             tmp = dialogBox.GetComponentInChildren<TMP_Text>();
             tmp.text = dialog;
 
@@ -62,7 +68,7 @@ public class DialogHandler : MonoBehaviour
             tmp = characterBox.GetComponentInChildren<TMP_Text>();
             tmp.text = characterName;
 
-            var imager = GameObject.FindWithTag("CharacterImage").GetComponent<RawImage>();
+            var imager = GameObject.FindWithTag("CHI").GetComponent<RawImage>();
             imager.texture = chardata[characterIndex][emoIndex];
         }
         else
@@ -70,12 +76,12 @@ public class DialogHandler : MonoBehaviour
             var dialogBox = GameObject.FindWithTag("DialogBox");
             tmp = dialogBox.GetComponentInChildren<TMP_Text>();
             tmp.text = dialog;
-            
+
             var characterBox = GameObject.FindWithTag("CharacterNameBox");
             tmp = characterBox.GetComponentInChildren<TMP_Text>();
             tmp.text = characterName;
 
-            var imager = GameObject.FindWithTag("CharacterImage").GetComponent<RawImage>();
+            var imager = GameObject.FindWithTag("CHI").GetComponent<RawImage>();
             imager.texture = chardata[characterIndex][emoIndex];
         }
     }
@@ -86,6 +92,30 @@ public class DialogHandler : MonoBehaviour
         chardata.Add(vasuliEmo); // 1
         chardata.Add(ngoEmo); // 2
         chardata.Add(oppEmo); // 3
-        Dialog("haha complete HO GAYA!!!", 0, 1);
+
+        dialogs.Add("Make");
+        dialogs.Add("How");
+        dialogs.Add("W");
+        dialogs.Add("idk");
+
+        charintexes.Add(0);
+        charintexes.Add(0);
+        charintexes.Add(0);
+        charintexes.Add(0);
+
+        emoindexes.Add(2);
+        emoindexes.Add(0);
+        emoindexes.Add(1);
+        emoindexes.Add(1);
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space) && i < 4)
+        {
+            Dialog(dialogs[i], charintexes[i], emoindexes[i]);
+            i++;
+        }
     }
 }
