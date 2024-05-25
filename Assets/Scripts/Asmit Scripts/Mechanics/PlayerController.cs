0160a09c8c6b8d2f1b267aaa34513bef60be2d26
Jai Khurana <k.jai.khurana@gmail.com>
2024-05-25 23:42:32 +0530

    add canvas notifications for overcoooked system

--- Assets/Scripts/Asmit Scripts/Mechanics/PlayerController.cs
+++ Assets/Scripts/Asmit Scripts/Mechanics/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Burst.Intrinsics;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -10,6 +11,8 @@ public class PlayerControl : MonoBehaviour
 {
     public GameObject teaCup;
     public GameObject teaSlot;
+    public GameObject teaSlot1;
+    public GameObject teaSlot2;
     public GameObject parleG;
     public GameObject parleGSlot;
     public GameObject arm;
@@ -21,18 +24,31 @@ public class PlayerControl : MonoBehaviour
     
     private bool isInFrontOfStall;
     private bool isInFrontOfTable;
+    private bool isInFrontOfTable1;
+    private bool isInFrontOfTable2;
 
     private int numberOfItemsInHand = 0;
     
     // public Rigidbody playerRb;
 
-    
+    public List<Transform> Teaslots = new List<Transform>();
     
     public float speed = 40.0f;
     public GameObject[] itemsInHand;
 
+    private void Start()
+    {
+       Teaslots.Add(teaSlot.transform);
+       Teaslots.Add(teaSlot1.transform);
+       Teaslots.Add(teaSlot2.transform);
+       
+    }
+
     private void Update()
     {
+        Debug.Log("table  " + isInFrontOfTable);
+        Debug.Log("table 1 " + isInFrontOfTable1);
+        Debug.Log("table 2 " + isInFrontOfTable2);
         float horizontal = Input.GetAxisRaw("Horizontal");
         Vector3 direction = new Vector3(horizontal, 0f, 0f);
         float horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
@@ -45,7 +61,6 @@ public class PlayerControl : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             gameObject.transform.rotation = new Quaternion(0, 180, 0,0 );
-
         } 
         if (Input.GetKeyDown(KeyCode.D))
         {
@@ -60,6 +75,7 @@ public class PlayerControl : MonoBehaviour
                 teaCup.transform.position = new Vector3(arm.transform.position.x,
                     arm.transform.position.y+0.1f, arm.transform.position.z);
                 numberOfItemsInHand++;
+                teaCup.transform.localScale = Vector3.one * 2.5f;
             }
            
             if (numberOfItemsInHand == 1 && Input.GetKeyDown(KeyCode.E))
@@ -88,6 +104,43 @@ public class PlayerControl : MonoBehaviour
                 numberOfItemsInHand--;
             }
         }
+        else if (isInFrontOfTable1 == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                teaCup.transform.parent = teaSlot1.transform;
+                teaCup.transform.position = new Vector3(teaSlot1.transform.position.x,
+                    teaSlot1.transform.position.y, teaSlot1.transform.position.z);
+                numberOfItemsInHand--;
+            }
+           
+            if (numberOfItemsInHand == 1 && Input.GetKeyDown(KeyCode.E))
+            {
+                parleG.transform.parent = parleGSlot.transform;
+                parleG.transform.position = new Vector3(parleGSlot.transform.position.x,
+                    parleGSlot.transform.position.y+0.1f, parleGSlot.transform.position.z);
+                numberOfItemsInHand--;
+            }
+        }
+        else if (isInFrontOfTable2 == true)
+        {
+            Debug.Log(teaSlot2.transform.position);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                teaCup.transform.parent = teaSlot2.transform;
+                teaCup.transform.position = new Vector3(teaSlot2.transform.position.x,
+                    teaSlot2.transform.position.y, teaSlot2.transform.position.z);
+                numberOfItemsInHand--;
+            }
+           
+            if (numberOfItemsInHand == 1 && Input.GetKeyDown(KeyCode.E))
+            {
+                parleG.transform.parent = parleGSlot.transform;
+                parleG.transform.position = new Vector3(parleGSlot.transform.position.x,
+                    parleGSlot.transform.position.y+0.1f, parleGSlot.transform.position.z);
+                numberOfItemsInHand--;
+            }
+        }
     }
     
     private void OnTriggerEnter(Collider other)
@@ -102,6 +155,14 @@ public class PlayerControl : MonoBehaviour
         {
             isInFrontOfTable = true;
         }
+        else if (other.name == "Table (1)")
+        {
+            isInFrontOfTable1 = true;
+        }
+        else if (other.name == "Table (2)")
+        {
+            isInFrontOfTable2 = true;
+        }
     }
 
     private void OnTriggerExit(Collider other)
@@ -113,6 +174,14 @@ public class PlayerControl : MonoBehaviour
         else if (other.name == "Table")
         {
             isInFrontOfTable = false;
+        }
+        else if (other.name == "Table (1)")
+        {
+            isInFrontOfTable1 = false;
+        }
+        else if (other.name == "Table (2)")
+        {
+            isInFrontOfTable2 = false;
         }
     }
 }
---
+++ Assets/banda.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class banda : MonoBehaviour
+{
+    [SerializeField]private List<GameObject> ChaiLaUI = new List<GameObject>();
+    [SerializeField]private List<GameObject> bande = new List<GameObject>();
+    void Update()
+    {
+        
+    }
+}
