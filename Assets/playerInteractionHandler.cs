8e7ae1c9ef2524273844e1e9b8f757163007217b
Jai Khurana <k.jai.khurana@gmail.com>
2024-05-25 10:30:28 +0530

    Update playerInteractionHandler.cs

--- Assets/playerInteractionHandler.cs
+++ Assets/playerInteractionHandler.cs
@@ -7,6 +7,11 @@ public class PlayerHandler : MonoBehaviour
     public int i = -1;
     public DialogHandler dialogHandler;
     public Owner owner;
+    public SpriteRenderer ground;
+
+    private bool canInteract = false;
+    private bool canOverhear = false;
+    private float distanceWith;
     void Start()
     {
         owner = (Owner)FindObjectsOfType(typeof(Owner))[0];
@@ -16,7 +21,30 @@ public class PlayerHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && i < 4)
+        var distancevector = owner.transform.position - gameObject.transform.position;
+        distanceWith = distancevector.magnitude;
+        
+        if (distanceWith < 0.3)
+        {
+           ground.color = Color.red;
+           canInteract = true;
+        }
+        else if (0.3 < distanceWith && distanceWith < 1.4)
+        {
+           ground.color = Color.yellow;
+           canOverhear = true;
+           
+           dialogHandler.CloseDialog();
+        }
+        else
+        {
+           ground.color = Color.white;
+           canInteract = false;
+           canOverhear = false;
+           
+           dialogHandler.CloseDialog();
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && i < 4 && canInteract == true)
         {
             i++;
             dialogHandler.Dialog(owner.dialogs[i], owner.charintexes[i], owner.emoindexes[i]);
